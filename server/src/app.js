91556d055155b2eab54e3322eb7388eb1d3f61bc
TERPHK <phakkharaphon23@gmail.com>
2023-08-09 11:00:51 +0700

    install express

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+let express = require('express')
+
+const app = express()
+
+ 
+
+app.get('/status', function (req,res){
+
+    res.send('Hello nodejs server 12345')
+
+})
+
+ 
+
+let port = 8081
+
+app.listen(port, function(){
+
+    console.log('server running on ' + port)
+
+})
