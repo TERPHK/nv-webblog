4bb1ae9c246eb2cec87f16fe44e63f711aaf1dea
TERPHK <phakkharaphon23@gmail.com>
2023-08-16 09:49:11 +0700

    create user and edit information

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,49 @@
 let express = require('express')
+let bodyParser = require('body-parser')
 
 const app = express()
 
- 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/status', function (req,res){
-
-    res.send('Hello nodejs server nodemon')
-
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server')
 })
 
-
-app.get('/hello/:name', function (req,res) {
+app.get('/hello/:person', function (req, res) {
     console.log('hello - ' + req.params.person)
     res.send('sey hello with ' + req.params.person)
 })
- 
+
+// get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
+})
+
+// get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกใช้ข้อมูลผู้ใช้งานทั้งหมด')
+})
+
+
+
+// edit user
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + JSON.stringify(req.body))
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทําการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
+        JSON.stringify(req.body))
+    
+})
+// create user
+app.post('/user',function(req,res){
+    res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
+})
 
 let port = 8081
-
-app.listen(port, function(){
-
+app.listen(port, function () {
     console.log('server running on ' + port)
-
 })
