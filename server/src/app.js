4b8c6e5601bb6c277c810538a218b0fccae89af0
TERPHK <phakkharaphon23@gmail.com>
2023-09-13 11:06:33 +0700

    try,async

--- server/src/app.js
+++ server/src/app.js
@@ -12,42 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./routes')(app)
 
-app.get('/status', function (req, res) {
-    res.send('Hello nodejs server')
-})
 
-app.get('/hello/:person', function (req, res) {
-    console.log('hello - ' + req.params.person)
-    res.send('sey hello with ' + req.params.person)
-})
-
-// get user by id
-app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
-})
-
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกใช้ข้อมูลผู้ใช้งานทั้งหมด')
-})
-
-
-
-// edit user
-app.put('/user/:userId', function (req, res) {
-    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + JSON.stringify(req.body))
-})
-
-// delete user
-app.delete('/user/:userId', function (req, res) {
-    res.send('ทําการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
-        JSON.stringify(req.body))
-
-})
-// create user
-app.post('/user', function (req, res) {
-    res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
-})
 
 let port = process.env.PORT || config.port
 sequelize.sync({ force: false }).then(() => {
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,7 @@
 const UserController = require('./controllers/UserController')
+
 module.exports = (app) => {
-    /* RESFUL Api for users management */
+    
     // create user
     app.post('/user',
         UserController.create
